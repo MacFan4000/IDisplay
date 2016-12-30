21f5a1dbaa038ce80101655c0d417d7d59f83f4d
Brent Laabs <blaabs@perceptyx.com>
2016-12-30 01:59:12 -0800

    Use Html class to eliminate XSS vulnerabilities, simplify
    I don't think that interstitial works yet, but this is still
    a vast improvement for security.

--- iDisplay.php
+++ iDisplay.php
@@ -20,49 +20,37 @@ $wgHooks['ParserFirstCallInit'][] = 'idisplay_Setup';
 # Add a hook to initialise the magic word
 $wgHooks['LanguageGetMagic'][]       = 'idisplay_Magic';
 
-function idisplay_Setup( &$parser ) 
-	{
+function idisplay_Setup( &$parser ) {
     # Set a function hook associating the magic word with our function
 	$parser->setFunctionHook( 'idisplay', 'idisplay_Render' );
     return true;
-	}
-function idisplay_Magic( &$magicWords, $langCode ) 
-	{
+}
+
+function idisplay_Magic( &$magicWords, $langCode ) {
     # Add the magic word
     # The first array element is whether to be case sensitive, in this case (0) it is not case sensitive, 1 would be sensitive
     # All remaining elements are synonyms for our parser function
     $magicWords['idisplay'] = array( 0, 'idisplay', 'anysite' );
     # unless we return true, other parser functions extensions won't get loaded.
     return true;
-	}
-function idisplay_Render( $parser, $input, $w='', $h='', $s='') 
-	{
-        if ($w!='')
-			{
-			$width =$w;
-			} 
-		else 
-			{
-			$width = 800;
-			}
-		if ($h!='') 
-			{
-			$height = $h;
-			} 
-		else
-			{
-			$height = 600;
-			}
-		if($s!='')
-			{
-			//intersite here! give it the input!
-			$input = str_replace("&","[",$input);
-			$output= '<iframe name="idisplay" src="http://fh-hfv-web02.hf.brnet.int/at-wiki/extensions/iDisplay/inter.php?page='.$input.'" width="'.$width.'" height="'.$height.'" frameborder="0">'.'</iframe>'; 
-			}
-		else
-			{
-			$output= '<iframe name="idisplay" src="'.$input.'" width="'.$width.'" height="'.$height.'" frameborder="0">'.'</iframe>'; 
-			}		    
-	 return array($output, 'noparse' => true, 'isHTML' => true);
-	}
+}
+function idisplay_Render( $parser, $input, $w='', $h='', $s='') {
+    $width  = (int)$w > 0 ? (int)$w : 800;
+    $height = (int)$h > 0 ? (int)$h : 600;
+
+    if($s!='') {
+        //intersite here! give it the input!
+        $input = 'http://fh-hfv-web02.hf.brnet.int/at-wiki/extensions/iDisplay/inter.php?page='
+            . str_replace("&","[",$input);
+    }
+        
+    $output = Html::element( 'iframe', array(
+        'width'  => $width,
+        'height' => $height,
+        'src'    => $input,
+        'frameborder' => "0",
+    ));
+        
+    return array($output, 'noparse' => true, 'isHTML' => true);
+}
 ?>
